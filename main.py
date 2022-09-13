8a8f1fc17bf8ad10f939852adf1756b568afcf24
dsshv <ds.shvts@gmail.com>
2022-09-13 19:36:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import paramiko
+import os
+
+SERVER_DIR = '/home/student/Students/Shevtsov/lesson2/'
+HOME_DIR = '/home/dsshv/PycharmProjects/supercomp_lesson2/'
+
+
+def set_ssh_connection():
+    ssh_connection = paramiko.SSHClient()
+    ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_connection.connect(
+        hostname='195.208.250.3',
+        username='student',
+        password='Xanes2000'
+    )
+    return ssh_connection
+
+
+def sftp_connection_op(ssh_connection):
+    sftp = ssh_connection.open_sftp()
+    cwd = os.getcwd()
+    files = os.listdir(cwd)
+    print(files)
+    ssh_connection.exec_command(f'cd ./{SERVER_DIR}')
+    file_to_upload = './test.txt'
+    file_to_download = f'{SERVER_DIR}test2.txt'
+    place_to_save = 'test2.txt'
+    sftp.put(file_to_upload, f'{SERVER_DIR}/text.txt')
+    sftp.get(file_to_download, place_to_save)
+    print('sftp op done')
+    sftp.close()
+
+
+def os_op(ssh_connection):
+    stdin, stdout, stderr = ssh_connection.exec_command(f'ls {SERVER_DIR}')
+    ls_dir = stdout.readlines()
+    print(ls_dir)
+    ssh_connection.exec_command(f'cd ./{SERVER_DIR}')
+    stdin, stdout, stderr = ssh_connection.exec_command(f'ls -> files_list_in_this_dir')
+    list_files = stdout.readlines()
+
+
+if __name__ == '__main__':
+    ssh = set_ssh_connection()
+    os.chdir(HOME_DIR)
+    # ssh = paramiko.SSHClient()
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # ssh.connect(
+    #     hostname='195.208.250.3',
+    #     username='student',
+    #     password='Xanes2000'
+    # )
+    # stdin, stdout, stderr = ssh.exec_command(f'cd {SERVER_DIR}')
+    # stdin, stdout, stderr = ssh.exec_command(f'ls')
+    # li = stdout.readlines()
+    # print(li);
+    sftp = ssh.open_sftp()
+    sftp_connection_op(ssh)
+    os_op(ssh)
+    ssh.close()
