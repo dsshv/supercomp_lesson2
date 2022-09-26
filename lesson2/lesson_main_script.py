801fb5fd905e4ea4c5a647ddd89b66e01248cb44
dsshv <ds.shvts@gmail.com>
2022-09-26 22:36:17 +0300

    start lesson 2

--- lesson2/lesson_main_script.py
+++ lesson2/lesson_main_script.py
@@ -1,9 +1,43 @@
+import altair
+
 from instruments.dataframe_instruments import get_dataframe_from_file
 import pandas as pd
+import altair as alt
+import seaborn as sns
+
+import matplotlib.pyplot as plt
 
 
 def lesson_2():
-
     def graph_task():
-        text_file = './input_files/out.txt'
+        text_file = './lesson2/input_files/out.txt'
         dataframe = get_dataframe_from_file(text_file, 2)
+        dataframe.rename(
+            columns={0: 'Energy', 1: '<xanes>'},
+            inplace=True
+        )
+        print(dataframe)
+
+        plt.plot(dataframe['Energy'], dataframe['<xanes>'])
+        plt.show()
+
+    def normalize_task():
+        text_file = './lesson2/input_files/out.txt'
+        dataframe = get_dataframe_from_file(text_file, 2)
+        dataframe.rename(
+            columns={0: 'Energy', 1: '<xanes>'},
+            inplace=True
+        )
+        plt.plot(dataframe['Energy'], dataframe['<xanes>'])
+        plt.show()
+
+        min_y = dataframe['<xanes>'].min()
+        dataframe['<xanes>'] -= min_y
+        max_y = dataframe['<xanes>'].max()
+        dataframe['<xanes>'] /= max_y
+
+        plt.plot(dataframe['Energy'], dataframe['<xanes>'])
+        plt.show()
+
+    normalize_task()
+    # graph_task()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import lesson1.lesson_main_script as l1
-
+import lesson2.lesson_main_script as l2
 
 if __name__ == '__main__':
-    l1.lesson_1()
+    # l1.lesson_1()
+    l2.lesson_2()
